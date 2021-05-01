c1845a1113581e4d5ff4695706ed6f9a7b9c9c11
marciodegan <marciodegan@yahoo.com>
2021-04-30 22:18:02 -0300

    setup

---
+++ src/test/java/br/com/zupacademy/marciodegan/propostas/PropostasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.marciodegan.propostas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
